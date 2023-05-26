e2bd892bd4b3d54631afb1290ac0e2b622bec981
Doug Stensrud <dwsmitpython@gmail.com>
2023-05-25 21:23:21 -0700

    pass test-4

--- exercises/e4.js
+++ exercises/e4.js
@@ -4,12 +4,34 @@
 // findMaxValueInArray([2, 45, 32, 3, 0, 46, 12]) => 46
 
 export function findMinValueInArray(array) {
-  // Your code goes here...
+  let x = 0;
+  let minVal = null;
+  for (x of array) {
+    if( x < minVal || minVal == null) {
+      minVal = x;
+    }
+  }
+  return minVal;
 }
 
 export function findMaxValueInArray(array) {
-  // Your code goes here...
+  let xx = 0;
+  let maxVal = null;
+  for (xx of array) {
+    if( xx > maxVal ) {
+      maxVal = xx;
+    }
+  }
+  return maxVal;
 }
+let arr = [2, 45, 32, 3, 0, 46, 12];
+console.log(findMinValueInArray(arr));
+console.log(findMaxValueInArray(arr));
+
+let arr2 = [20, 24, 19, 26];
+console.log(findMinValueInArray(arr2));
+console.log(findMaxValueInArray(arr2));
+
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-4"
