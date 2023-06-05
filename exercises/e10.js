d2152ee9ecdaaa29c8724e20ea5726aa95fabfc3
Doug Stensrud <dwsmitpython@gmail.com>
2023-06-05 09:23:54 -0700

    pass test 10

--- exercises/e10.js
+++ exercises/e10.js
@@ -3,10 +3,47 @@
 // Array example: bankAccounts in /data/data.js
 // getClientsWithLetterInName(bankAccounts, 'e') => ['Kevin', 'Steven', ...]
 
-export function getClientsWithLetterInName(array, letter) {
-  // Your code goes here...
+const bankAccounts = [
+  {
+    id: 1,
+    name: "Susan",
+    balance: 100.32,
+    deposits: [150, 30, 221],
+    withdrawals: [110, 70.68, 120],
+  },
+  { id: 2, name: "Morgan", balance: 1100.0, deposits: [1100] },
+  {
+    id: 3,
+    name: "Joshua",
+    balance: 18456.57,
+    deposits: [4000, 5000, 6000, 9200, 256.57],
+    withdrawals: [1500, 1400, 1500, 1500],
+  },
+  { id: 4, name: "Candy", balance: 0.0 },
+  { id: 5, name: "Phil", balance: 18, deposits: [100, 18], withdrawals: [100] },
+];
 
+
+export function getClientsWithLetterInName( array, letter ) {
+  let names = [];
+  let namesWithLetter = [];
+  for ( let account of array ) {
+    names.push(account.name)
+    // console.log(names)
+    }
+  for ( let person of names ) {
+    if (person.includes(letter.toLowerCase()) || person.includes(letter.toUpperCase())) {
+      namesWithLetter.push(person);
+    }
+  }
+  // return names;
+  console.log(namesWithLetter);
+  return namesWithLetter;
 }
+
+let letter = 'c';
+getClientsWithLetterInName( bankAccounts, letter);
+// console.log(getClientsWithLetterInName( bankAccounts, letter));
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-10"
